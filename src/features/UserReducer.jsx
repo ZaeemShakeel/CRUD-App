b49795eee708ab99910623b201bb11a3a4113852
ZaeemShakeel <zaeemshakeel.zs2002@gmail.com>
2026-06-13 15:17:36 +0500

    By Zaeem, comment

--- src/features/UserReducer.jsx
+++ src/features/UserReducer.jsx
@@ -2,6 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 import { userList } from "../utils/Data";
 import Update from "../components/Update";
 
+// This is reducer
+
 const userSlice = createSlice({
   name: "users",
   initialState: userList,
